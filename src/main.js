7c4ffce17b6130f3e60b31ff70bf5dcecb23502e
Michael Korpi <mhkorpi@gmail.com>
2023-05-16 18:31:59 -0700

    added changes from lesson 17.4

--- src/main.js
+++ src/main.js
@@ -3,3 +3,17 @@ const { welcome, goodbye, tell } = require("../utils/fortune-teller");
 const promise = welcome();
 
 console.log(promise);
+
+const question = "Will the weather be nice today?";
+const tellPromise = tell();
+
+tellPromise
+    .then((fortune) => {
+        console.log(question);
+        console.log(fortune);
+    })
+    .catch((error) => {
+        console.log(error)
+    });
+    //***OR***
+    //.catch(console.error)
